4bd9429849b056c2f3d3e627d39f8f5b2ca49fcc
jwoodii <35505841+jwoodii@users.noreply.github.com>
2018-04-12 18:36:14 -1000

    changed

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -9,9 +9,9 @@ import { Roles } from 'meteor/alanning:roles';
 /** The NavBar appears at the top of every page. Rendered by the App Layout component. */
 class NavBar extends React.Component {
   render() {
-    const menuStyle = { marginBottom: '10px' };
+    const menuStyle = { marginBottom: '10px', backgroundColor: '#024731' };
     return (
-      <Menu color={'teal'} style={menuStyle} attached="top" borderless inverted>
+      <Menu style={menuStyle} attached="top" borderless inverted>
         <Menu.Item as={NavLink} activeClassName="" exact to="/">
           <img className="ui fitted image item"
                src='/images/ManoaOverflow.png'/>
