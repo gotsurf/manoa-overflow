0e88c61745cb4dc35281068e2f6ea38f258a8abb
jwoodii <35505841+jwoodii@users.noreply.github.com>
2018-04-17 18:26:03 -1000

    added routes and fixed navbar

--- app/imports/ui/components/NavBar.jsx
+++ app/imports/ui/components/NavBar.jsx
@@ -16,9 +16,12 @@ class NavBar extends React.Component {
             <img className="ui fitted image item"
                  src='/images/ManoaOverflow.png'/>
           </Menu.Item>
-          <Menu.Item as={NavLink}activeClassName="active" exact to="/myCourses" key='myCourses'>My Courses</Menu.Item>
-          <Menu.Item as={NavLink}activeClassName="active" exact to="/myQuestions" key='myQuestions'>My Questions</Menu.Item>
-          <Menu.Item as={NavLink}activeClassName="active" exact to="/myAnswers" key='myAnswers'>My Answers</Menu.Item>
+          {this.props.currentUser ? (
+          <Menu.Item as={NavLink} activeClassName="active" exact to="/myCourses" key='myCourses'>My Courses</Menu.Item>,
+          <Menu.Item as={NavLink} activeClassName="active" exact to="/myQuestions" key='myQuestions'>My
+            Questions</Menu.Item>,
+          <Menu.Item as={NavLink} activeClassName="active" exact to="/myResponses" key='myResponses'>My Responses</Menu.Item>
+              ) : ''}
           {Roles.userIsInRole(Meteor.userId(), 'admin') ? (
               <div className="ui left item"><Menu.Item as={NavLink} activeClassName="active" exact to="/admin"
                                                        key='admin'>Admin</Menu.Item></div>
