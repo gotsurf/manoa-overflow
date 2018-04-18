9046bfc1055d1ff2d236beaaf9d302fbbcc9935e
Matt Lam <mwklam@hawaii.edu>
2018-04-17 18:09:21 -1000

    myanswers

--- app/imports/ui/layouts/App.jsx
+++ app/imports/ui/layouts/App.jsx
@@ -19,6 +19,7 @@ import Signout from '../pages/Signout';
 import QuestionMockup from '../pages/QuestionMockup';
 import CoursePageMockup from '../pages/CoursePageMockup';
 import MyQuestions from '../pages/MyQuestions';
+import MyAnswers from '../pages/MyAnswers';
 
 /** Top-level layout component for this application. Called in imports/startup/client/startup.jsx. */
 class App extends React.Component {
@@ -34,6 +35,7 @@ class App extends React.Component {
               <Route path="/examplequestion" component={QuestionMockup}/>
               <Route path="/examplecourse" component={CoursePageMockup}/>
               <Route path="/my-questions" component={MyQuestions}/>
+              <Route path="/my-answers" component={MyAnswers}/>
               <ProtectedRoute path="/list" component={ListStuff}/>
               <ProtectedRoute path="/add" component={AddStuff}/>
               <ProtectedRoute path="/edit/:_id" component={EditStuff}/>
